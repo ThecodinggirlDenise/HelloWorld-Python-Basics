da297949272e390ba10a99be5a3235e27787c0d6
Deniz <136066030+ThecodinggirlDenise@users.noreply.github.com>
2025-06-24 00:44:43 +0300

    the Baisc P

---
+++ app.py
@@ -0,0 +1,10 @@
+
+#name=("Janet Brown"
+# )
+#if len(name) < 3:
+#   print("Name must be longer than 3 characters")
+#elif len(name) > 20:  # Fix indentation here
+#  print("Name must be shorter than 50 characters")
+#else:
+    #print("It looks amazing")
+weight=int(input("Enter your weight: "))
